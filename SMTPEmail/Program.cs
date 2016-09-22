0a7e668f4ec6bbbf81f9a3a209ea6387a2197d1c
Cheng Cui <cuicheng11165@gmail.com>
2016-09-22 17:09:12 +0800

    use netmail

--- SMTPEmail/Program.cs
+++ SMTPEmail/Program.cs
@@ -1,8 +1,9 @@
-﻿using EAGetMail;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 
 namespace SMTPEmail
@@ -11,22 +12,62 @@ namespace SMTPEmail
     {
         static void Main(string[] args)
         {
-            ImapClient ic = new ImapClient("imap.gmail.com", "name@gmail.com", "pass",
-                   ImapClient.AuthMethods.Login, 993, true);
-            // Select a mailbox. Case-insensitive
-            ic.SelectMailbox("INBOX");
-            Console.WriteLine(ic.GetMessageCount());
-            // Get the first *11* messages. 0 is the first message;
-            // and it also includes the 10th message, which is really the eleventh ;)
-            // MailMessage represents, well, a message in your mailbox
-            MailMessage[] mm = ic.GetMessages(0, 10);
-            foreach (MailMessage m in mm)
-            {
-                Console.WriteLine(m.Subject);
-            }
-            // Probably wiser to use a using statement
-            ic.Dispose();
+            var server = new SmtpServer { Host = "smtp.qq.com", Username = "cuicheng11165@qq.com", Password = "password,", Port = 25, Sender = "cuicheng11165@qq.com", UseSSL = true };
+            var smtpClient = GetSmtpClient(server);
+            ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
+
+
+            smtpClient.Send(server.Sender, "cuicheng11165@qq.com", "123", "222");
 
         }
+
+        private static SmtpClient GetSmtpClient(SmtpServer setting)
+        {
+
+            var client = new SmtpClient()
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Host = setting.Host,
+                EnableSsl = setting.UseSSL,
+                Timeout = 18000000,
+            };
+
+            if (setting.Port > 0 && setting.Port < 65536)
+            {
+                client.Port = setting.Port;
+            }
+
+            if (string.IsNullOrEmpty(setting.Username) || string.IsNullOrEmpty(setting.Password))
+            {
+                client.UseDefaultCredentials = true;
+            }
+            else
+            {
+                var credetntial = new NetworkCredential(setting.Username, setting.Password);
+                client.UseDefaultCredentials = false;
+                client.Credentials = credetntial;
+            }
+
+            return client;
+        }
+    }
+
+    public class SmtpServer
+    {
+
+        public int Port { get; set; }
+
+        public string Host { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public bool SecurePassword { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
     }
 }
